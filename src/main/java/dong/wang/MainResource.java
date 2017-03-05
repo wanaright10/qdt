856aa0e2ab347df3f984b7b7241113485fee0c46
HU Liting <452304845@qq.com>
2017-03-05 15:57:15 +0800

    add new api call

--- src/main/java/dong/wang/MainResource.java
+++ src/main/java/dong/wang/MainResource.java
@@ -1,14 +1,14 @@
 package dong.wang;
 
 import com.github.kevinsawicki.http.HttpRequest;
+import javafx.util.Pair;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.SecureRandom;
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @RequestMapping("/check")
@@ -16,16 +16,34 @@ public class MainResource {
     private static final int attendancePointAddressID = 8952;
     private static final int companyID = 15538;
     private static final int employeeID = 57569;
-    private static final List<String> addressPool = Arrays.asList("天华二路201号附近", "天华二路181号附近", "世纪城南路附近", "天府软件园D区D2楼附近");
+    private static final String HOST = "http://service.qdtsoft.com";
 
     @RequestMapping(value = "/out", method = RequestMethod.GET)
-    public HttpEntity<String> checkOut() {
+    public HttpEntity<String> checkOut(@RequestParam(required = false, defaultValue = "false") Boolean callNew) {
+        if (callNew) {
+            return checkOutNew();
+        } else {
+            return checkOutOld();
+        }
+    }
+
+
+    @RequestMapping(value = "/in", method = RequestMethod.GET)
+    public HttpEntity<String> checkIn(@RequestParam(required = false, defaultValue = "false") Boolean callNew) {
+        if (callNew) {
+            return checkInNew();
+        } else {
+            return checkInOld();
+        }
+    }
+
+    private HttpEntity<String> checkOutOld() {
         int precision = new SecureRandom().nextInt(30) + 1;
         int distance = new SecureRandom().nextInt(250) + 1;
-        String address = randomAddress() + " (精确到" + precision + ".00米) 距离考勤点" + distance + "米";
+        String address = Util.getRandomAddress(false) + " (精确到" + precision + ".00米) 距离考勤点" + distance + "米";
 
-        String result = HttpRequest.post("http://service.qdtsoft.com/AttendanceService/AttendanceCheckOut")
-                .header("Content-Type", "application/json")
+        String result = HttpRequest.post(HOST + "/AttendanceService/AttendanceCheckOut")
+                .headers(Util.getHeaders())
                 .send("{\n" +
                         "\t\"attendanceCheckOutInputValue\": {\n" +
                         "\t\t\"Address\": \"" + address + "\",\n" +
@@ -45,14 +63,13 @@ public class MainResource {
         return new HttpEntity<>(result);
     }
 
-    @RequestMapping(value = "/in", method = RequestMethod.GET)
-    public HttpEntity<String> checkIn() {
+    private HttpEntity<String> checkInOld() {
         int precision = new SecureRandom().nextInt(30) + 1;
         int distance = new SecureRandom().nextInt(250) + 1;
-        String address = randomAddress() + " (精确到" + precision + ".00米) 距离考勤点" + distance + "米";
+        String address = Util.getRandomAddress(false) + " (精确到" + precision + ".00米) 距离考勤点" + distance + "米";
 
-        String result = HttpRequest.post("http://service.qdtsoft.com/AttendanceService/AttendanceCheckIn")
-                .header("Content-Type", "application/json")
+        String result = HttpRequest.post(HOST + "/AttendanceService/AttendanceCheckIn")
+                .headers(Util.getHeaders())
                 .send("{\n" +
                         "\t\"attendanceCheckInInputValue\": {\n" +
                         "\t\t\"Address\": \"" + address + "\",\n" +
@@ -72,7 +89,65 @@ public class MainResource {
         return new HttpEntity<>(result);
     }
 
-    private String randomAddress() {
-        return addressPool.get(new SecureRandom().nextInt(addressPool.size()));
+    // ==================   新的接口  ========================
+
+    private HttpEntity<String> checkOutNew() {
+        Pair<String, String> randomAndToken = Util.getAPIRandomAndToken();
+        String address = Util.getRandomAddress(true);
+        Pair<String, String> location = Util.getLocation(address);
+
+        String result = HttpRequest.post(HOST + "/AttendanceService/AttendanceCheckOutWithPhotoKeys")
+                .headers(Util.getHeaders())
+                .send("{\n" +
+                        "    \"input\": {\n" +
+                        "        \"SpaceUsage\": 0.0,\n" +
+                        "        \"Address\": \"" + address + "\",\n" +
+                        "        \"AttendancePointAddressID\": " + attendancePointAddressID + ",\n" +
+                        "        \"Description\": \"\",\n" +
+                        "        \"Distance\": " + Util.getRandomDistance() + ",\n" +
+                        "        \"Latitude\": " + location.getKey() + ",\n" +
+                        "        \"Longtitude\": " + location.getValue() + ",\n" +
+                        "        \"Number\": 1,\n" +
+                        "        \"CompanyID\": " + companyID + ",\n" +
+                        "        \"EmployeeID\": " + employeeID + ",\n" +
+                        "        \"MenuID\": 0,\n" +
+                        "        \"OperatorID\": " + employeeID + ",\n" +
+                        "        \"Random\": \"" + randomAndToken.getKey() + "\",\n" +
+                        "        \"Token\": \"" + randomAndToken.getValue() + "\"\n" +
+                        "    }\n" +
+                        "}")
+                .body();
+
+        return new HttpEntity<>(result);
+    }
+
+    private HttpEntity<String> checkInNew() {
+        Pair<String, String> randomAndToken = Util.getAPIRandomAndToken();
+        String address = Util.getRandomAddress(true);
+        Pair<String, String> location = Util.getLocation(address);
+
+        String result = HttpRequest.post(HOST + "/AttendanceService/AttendanceCheckInWithPhotoKeys")
+                .headers(Util.getHeaders())
+                .send("{\n" +
+                        "    \"input\": {\n" +
+                        "        \"SpaceUsage\": 0.0,\n" +
+                        "        \"Address\": \"" + address + "\",\n" +
+                        "        \"AttendancePointAddressID\": " + attendancePointAddressID + ",\n" +
+                        "        \"Description\": \"\",\n" +
+                        "        \"Distance\": " + Util.getRandomDistance() + ",\n" +
+                        "        \"Latitude\": " + location.getKey() + ",\n" +
+                        "        \"Longtitude\": " + location.getValue() + ",\n" +
+                        "        \"Number\": 1,\n" +
+                        "        \"CompanyID\": " + companyID + ",\n" +
+                        "        \"EmployeeID\": " + employeeID + ",\n" +
+                        "        \"MenuID\": 0,\n" +
+                        "        \"OperatorID\": " + employeeID + ",\n" +
+                        "        \"Random\": \"" + randomAndToken.getKey() + "\",\n" +
+                        "        \"Token\": \"" + randomAndToken.getValue() + "\"\n" +
+                        "    }\n" +
+                        "}")
+                .body();
+
+        return new HttpEntity<>(result);
     }
 }
---
+++ src/main/java/dong/wang/Util.java
@@ -0,0 +1,91 @@
+package dong.wang;
+
+import javafx.util.Pair;
+
+import java.security.SecureRandom;
+import java.util.*;
+
+/**
+ * Created by Victor Wang.
+ * Created on 17/3/5 14:14
+ */
+public final class Util {
+    private static Map<String, String> headers = initHeaders();
+    private static final List<String> addressPoolOld = Arrays.asList("天华二路201号附近", "天华二路181号附近", "世纪城南路附近", "天府软件园D区D2楼附近");
+    private static final List<String> addressPoolNew = Arrays.asList("四川省成都市武侯区天华二路201号", "四川省成都市武侯区天华二路181号", "四川省成都市武侯区世纪城南路", "四川省成都市武侯区天府软件园D区D2楼");
+    private static Map<String, String> randomAndToken = initRandomAndToken();
+    private static Map<String, Pair<String, String>> location = initLocation();
+
+
+    private Util() {
+    }
+
+    private static Map<String, Pair<String, String>> initLocation() {
+        location = new HashMap<>();
+        location.put(addressPoolNew.get(0), new Pair<>("3.0542350253572105E7", "1.0407342196834784E8"));
+        location.put(addressPoolNew.get(1), new Pair<>("3.0542442653572105E7", "1.0407300353834784E8"));
+        location.put(addressPoolNew.get(2), new Pair<>("3.0541435463572105E7", "1.0407341123834784E8"));
+        location.put(addressPoolNew.get(3), new Pair<>("3.0540308143572105E7", "1.0407463431834784E8"));
+        return location;
+    }
+
+    private static Map<String, String> initRandomAndToken() {
+        randomAndToken = new HashMap<>();
+        randomAndToken.put("wVTI50", "B9B6CA567A3D9C0FAC693137E7C5F27E");
+        randomAndToken.put("eYAtCY", "F71E4805FA3A3023C6B864DD56AD23B8");
+        randomAndToken.put("83hyU5", "4039606CC0D2DE48C017A8267C5D01D3");
+        randomAndToken.put("2619s3", "62625E620E27034D30A8DB09FA95A71D");
+        randomAndToken.put("eYAtCY", "F71E4805FA3A3023C6B864DD56AD23B8");
+        return randomAndToken;
+    }
+
+    private static Map<String, String> initHeaders() {
+        headers = new HashMap<>();
+        headers.put("Content-Type", "application/json;charset=utf-8");
+        headers.put("User-Agent", "Dalvik/2.1.0 (Linux; U; Android 6.0.1; MI 4LTE MIUI/V8.2.1.0.MXDCNDL)");
+        headers.put("Host", "service.qdtsoft.com");
+        return headers;
+    }
+
+    public static Map<String, String> getHeaders() {
+        if (headers == null) {
+            headers = initHeaders();
+        }
+        return headers;
+    }
+
+    public static String getRandomAddress(boolean isNew) {
+        SecureRandom random = new SecureRandom();
+        List<String> addressPool;
+        if (isNew) {
+            addressPool = addressPoolNew;
+        } else {
+            addressPool = addressPoolOld;
+        }
+        return addressPool.get(random.nextInt(addressPool.size()));
+    }
+
+    public static int getRandomDistance() {
+        return new SecureRandom().nextInt(250) + 1;
+    }
+
+    public static Pair<String, String> getAPIRandomAndToken() {
+        if (randomAndToken == null) {
+            randomAndToken = initRandomAndToken();
+        }
+
+        SecureRandom random = new SecureRandom();
+        List<String> keys = new LinkedList<>(randomAndToken.keySet());
+        String key = keys.get(random.nextInt(keys.size()));
+
+        return new Pair<>(key, randomAndToken.get(key));
+    }
+
+    public static Pair<String, String> getLocation(String address) {
+        if (location == null) {
+            location = initLocation();
+        }
+
+        return location.get(address);
+    }
+}
